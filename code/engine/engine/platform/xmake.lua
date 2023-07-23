7cf7612748bdf1db323cc985238446ec4b38d28d
2481814748@qq.com <2481814748@qq.com>
2023-07-23 22:20:28 +0800

    add validation layer

--- code/engine/engine/platform/xmake.lua
+++ code/engine/engine/platform/xmake.lua
@@ -1,13 +1,15 @@
 -- vulkan
-add_requires("vulkan-validationlayers", {debug = true})
-add_requires("vulkansdk", "vulkan-loader")
+add_requires("vulkan-hpp", "vulkansdk", "vulkan-loader")
+add_requires("vulkan-validationlayers")
 
 -- vulkan and opengl
 add_requires("glm", "glfw")
 
 target("platform")
     add_deps("core")
-    add_packages("vulkansdk", "vulkan-loader", "vulkan-validationlayers", "glm", "glfw", {public = true}) 
+    add_packages("vulkan-hpp", "vulkansdk", "vulkan-loader", {public = true}) 
+    add_packages("vulkan-validationlayers", {public = true})
+    add_packages("glm", "glfw", {public = true})
 
     set_kind("static")
 
